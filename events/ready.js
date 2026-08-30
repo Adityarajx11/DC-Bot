e24c4b9b673890690051aaed87311f70a723408b
RAVENX <adixlucifer.0011@gmail.com>
2026-08-30 14:34:46 +0530

    Add database initialization to ready event

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,13 @@
 const { startLivePolling } = require('../lib/youtubeLive');
 const { initManager } = require('../lib/lavalink');
+const { initDatabase } = require('../lib/db');
 
 module.exports = {
   name: 'ready',
   once: true,
   async execute(client) {
     console.log(`✅ Logged in as ${client.user.tag}`);
+    await initDatabase();
     startLivePolling(client);
     initManager(client);
   },
